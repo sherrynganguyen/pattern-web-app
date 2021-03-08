40d422047684d2d9aa068e19bd6e319365675247
sherrynguyen2010 <ngasdragon@gmail.com>
2021-03-07 20:53:26 -0800

    get number from website and add comments

--- public/script/index.js
+++ public/script/index.js
@@ -1,37 +1,51 @@
 $(() => {
+
+  //Table template to display the rows of pattern
+
   const template = `
       <table class="data-table">
 
       </table>
       `;
   
+  //Submit button to get a random number
+
   $('.submit').click(function() {
-    $(".data-table").remove();
-     console.log("hello") 
-    const data = 4;
-    const initArray = [];
-    for (let i = 0; i < data; i++) {
-      initArray.push(data-i)
-    }
+    $(".data-table").remove(); //remove previous data when click button
+    let data;
+    $.ajax({
+      url: `https://foo-bar.azurewebsites.net/api/Numbers`,
+      dataType: 'json',
+      success: function (res) {
+        data = res.naturalNumber;
 
+        //Generate a array of the full pattern
+        const initArray = []; 
+        for (let i = 0; i < data; i++) {
+          initArray.push(data-i);
+        }
 
-    let finalArray = [];
+        //Generate a array of all rows of patterns
+        let finalArray = [];
 
-    for (let i = 1; i <= data; i++) {
-      let arrData1 = initArray.slice(0,i);
-      finalArray.push(arrData1)
-    }
+        for (let i = 1; i <= data; i++) {
+          let tempArray = initArray.slice(0,i); //Generate temporary arrays of pattern to add the finall array 
+          finalArray.push(tempArray);
+        }
 
-    const $temp = $(template)
+        //Create table for final array
+        const $temp = $(template);
 
-    for (let i = 0; i < finalArray.length; i++) {
-      $temp.append(`<tr class="row-${i+1}" ></tr>`)
-      for (let j = 0; j < finalArray[i].length; j++) {
-        $temp.find(`.row-${i+1}`).append(`<td> ${finalArray[i][j]}</td>`)
+        for (let i = 0; i < finalArray.length; i++) {
+          $temp.append(`<tr class="row-${i+1}" ></tr>`);
+          for (let j = 0; j < finalArray[i].length; j++) {
+            $temp.find(`.row-${i+1}`).append(`<td> ${finalArray[i][j]}</td>`);
+          }
+
+        }
+        $('.list').append($temp);
       }
+    })
 
-    }
-
-    $('.list').append($temp);
   });
 })
